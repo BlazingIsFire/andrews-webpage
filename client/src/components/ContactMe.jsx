d1ad98e48447d63e0415c5e03fa9a3b7a309e097
BlazingIsFire <firemanandy1102@gmail.com>
2022-11-23 12:39:43 -0500

    Created Contact Me section.

---
+++ client/src/components/ContactMe.jsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function ContactMe() {
+    return(
+        <div className='contact-container' id='contact'>
+            
+        </div>
+    )   
+}
+
+export default ContactMe;
