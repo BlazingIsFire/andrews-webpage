b61dc1bdf386910a179ff19857c9b0a419a2caa0
BlazingIsFire <firemanandy1102@gmail.com>
2022-11-23 12:30:07 -0500

    Modified to support mobile devices.

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,30 @@
-import { useState, useEffect } from 'react';
-import TextTransition, { presets } from 'react-text-transition';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
 import About from './components/About';
 import Skills from './components/Skills';
-import myAvatar from './images/avataaars.png';
+import Projects from './components/Projects';
+import ContactMe from './components/ContactMe';
 
 function App() {
-  const subtitleText = ['Software Engineer', 'Developer', 'Cyber Security Analyst', 'DoD Employee', 'Firefighter'];
-  const [subtitleIndex, setSubtitleIndex] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => 
-      setSubtitleIndex(subtitleIndex => subtitleIndex +1),
-      3000
-    );
-    return () => clearTimeout(intervalId)
-  }, []);
 
   return (
     <>
     <Navbar />
-    <div className='homepage-container' id='home'>
-      <div className='homepage-leftblock'>
-        <h4 className='homepage-introduction'>Hi, my name is</h4>
-        <h1 className='homepage-title'>Andrew Schweitzer</h1>
-        <div className='homepage-subtitle-container'>
-          <h3 className='homepage-subtitle'>I'm a</h3>
-          <TextTransition className='homepage-subtitle homepage-subtitle-texts' springConfig={presets.gentle}>{subtitleText[subtitleIndex % subtitleText.length]}</TextTransition>
-        </div>
+      <div className='homepage-container-web'>
+        <Homepage />
+        <About />
+        <Skills />
+        <Projects />
+        <ContactMe />
       </div>
-      <div className='homepage-rightblock'>
-        <img src={myAvatar} className='homepage-image'/>
-      </div>    
+    <div className='homepage-container-devices'>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/about' element={<About />}/>
+      </Routes>
     </div>
-    <About />
-    <Skills />
     </>
   );
 }
