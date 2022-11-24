d629a2c61c0fdca2ae9a840adaf498bfc5124ca0
BlazingIsFire <firemanandy1102@gmail.com>
2022-11-24 10:36:18 -0500

    Created ContactMe section of Homepage.

--- client/src/components/ContactMe.jsx
+++ client/src/components/ContactMe.jsx
@@ -1,10 +1,23 @@
-import '../App.css'
+import '../App.css';
+import { ReactComponent as LinkedInLogo } from '../images/linkedin.svg';
 
 function ContactMe() {
+    
     return(
-        <div className='contact-container' id='contact'>
-            
+        <>
+        <div className='contact-container' id='contactMe'>
+            <div className='contact-box'>
+                <h1>Want to connect?</h1>
+                <button id='contact-me-btn'>Contact Me </button>
+                <a>
+                    <LinkedInLogo title='LinkedIn Profile' id='linkedinprofile-svg'/>
+                </a>
+            </div>
+            <div className='contact-container-footer'>
+                <h7>Designed and built by <a href='https://github.com/BlazingIsFire' target='_blank' title='Github'>Andrew Schweitzer</a>.</h7>
+            </div>
         </div>
+        </>
     )   
 }
 
