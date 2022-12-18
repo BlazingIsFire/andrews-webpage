135c4fbd8c88182d8ddaf8e524792cc93e4a7f55
BlazingIsFire <firemanandy1102@gmail.com>
2022-12-18 17:44:58 -0500

    Created Dashboard page for updating user information and displaying account information. Currently a basic page with no styling.

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../App.css';
+
+function Dashboard() {
+    return(
+        <>
+            <h1>Account Dashboard!</h1>
+            <h1>Account Dashboard!</h1>
+            <h1>Account Dashboard!</h1>
+            <h1>Account Dashboard!</h1>
+            <h1>Account Dashboard!</h1>
+        </>
+    )
+}
+
+export default Dashboard;
